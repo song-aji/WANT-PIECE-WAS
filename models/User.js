aeacbd4b51669f6b08cb6cc168ce7f65cde02c63
Seungheok <50686751+Seungheok@users.noreply.github.com>
2024-09-04 17:43:54 +0900

    Update User.js
    수정본

--- models/User.js
+++ models/User.js
@@ -29,6 +29,9 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
